b337adcef48573cf2bcad1f3a45eb1c4b05d4d90
BySantana <diegodrcg@gmail.com>
2022-04-29 13:13:18 -0300

    Primeiro commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Aula40_wf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblEscolha = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(50, 129);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Formas de pagamento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Forma de pagamento:";
+            // 
+            // lblEscolha
+            // 
+            this.lblEscolha.AutoSize = true;
+            this.lblEscolha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEscolha.Location = new System.Drawing.Point(178, 183);
+            this.lblEscolha.Name = "lblEscolha";
+            this.lblEscolha.Size = new System.Drawing.Size(44, 16);
+            this.lblEscolha.TabIndex = 2;
+            this.lblEscolha.Text = "label3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(50, 254);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(50, 45);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(172, 22);
+            this.txtValor.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(50, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Valor: ";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(50, 293);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Visible = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(273, 357);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblEscolha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblEscolha;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula40_wf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Criar um ArrayList
+            ArrayList formaPagamento = new ArrayList();
+            formaPagamento.Add(new FormaDePagamento(1, "Selecione Opção"));
+            formaPagamento.Add(new FormaDePagamento(2, "Dinheiro"));
+            formaPagamento.Add(new FormaDePagamento(3, "Cartão"));
+            formaPagamento.Add(new FormaDePagamento(4, "Boleto"));
+            formaPagamento.Add(new FormaDePagamento(5, "Pix"));
+                                        
+            // Vincular ao comboBox1
+            comboBox1.DataSource = formaPagamento;
+            comboBox1.DisplayMember = "Descricao";
+            comboBox1.ValueMember = "ID";
+
+        }
+
+        public class FormaDePagamento
+        {
+            public int ID { get; set; }
+            public string Descricao { get; set; }
+
+            public FormaDePagamento(int id, string descricao)
+            {
+                this.ID = id;
+                this.Descricao = descricao;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int escolha = comboBox1.SelectedIndex;
+
+            switch (escolha)
+            {
+                case 1:
+                    lblEscolha.Text = "Dinheiro";
+                    textBox1.Visible = false;
+                    label3.Visible = false;
+                    btnCalcular.Visible = false;
+                    break;
+                case 2:
+                    lblEscolha.Text = "Cartão";
+                    label3.Text = "Quantidade de parcelas: ";
+                    textBox1.Visible = true;
+                    label3.Visible = true;
+                    btnCalcular.Visible = true;
+
+                    break;
+                case 3:
+                    lblEscolha.Text = "Boleto";
+                    textBox1.Visible = false;
+                    label3.Visible = false;
+                    btnCalcular.Visible = false;
+                    break;
+                case 4:
+                    lblEscolha.Text = "Pix";
+                    Random rnd = new Random();
+                    int numAl = rnd.Next(10000, 90000);
+                    textBox1.Text = numAl.ToString();
+                    label3.Text = "Chave PIX:";
+                    textBox1.Visible = true;
+                    label3.Visible = true;
+                    btnCalcular.Visible = false;
+                    break;
+                default:
+                    lblEscolha.Text = "";
+                    break;
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if(double.Parse(txtValor.Text) < 1000)
+            {
+                if(double.Parse(textBox1.Text) < 5)
+                {
+                    double parcelas = double.Parse(txtValor.Text) / double.Parse(textBox1.Text);
+                    MessageBox.Show($"Serão {double.Parse(textBox1.Text)} parcelas de {parcelas:F2}", "AVISO");
+                }
+                else
+                {
+                    MessageBox.Show("Número de parcelas não autorizado.");
+                }
+            }else if(double.Parse(txtValor.Text) >= 1000)
+            {
+                if (double.Parse(textBox1.Text) <= 10)
+                {
+                    double parcelas = double.Parse(txtValor.Text) / double.Parse(textBox1.Text);
+                    MessageBox.Show($"Serão {double.Parse(textBox1.Text)} parcelas de R${parcelas:F2}", "AVISO");
+                }
+                else
+                {
+                    MessageBox.Show("Número de parcelas não autorizado.");
+                }
+            }
+        }
+    }
+}
